6479a23729643d4dcc22dc79ee83744338d1e370
wbzhou1988 <wbzhou1988@163.com>
2017-07-01 15:45:44 +0800

    add sprite pics

--- js/script.js
+++ js/script.js
@@ -43,3 +43,15 @@ document.onkeydown = document.onkeyup = document.onkeypress = function() {
 		return (false);
 	}
 }
+window.onload=function(){
+	var submenuList = document.querySelectorAll(".submenuwithbg")[1];
+	var li = submenuList.children;
+	for(var i=0;i<li.length;i++){
+		var text = li[i].innerText.replace(/\s+/g,'').toLowerCase();
+		li[i].className = 'back '+text;
+	}
+}
+
+
+
+
