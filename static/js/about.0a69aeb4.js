7a1594a371f226dcc267e570d3e9df8cfa8cf674
wbzhou1988 <wbzhou1988@163.com>
2019-06-02 21:26:02 +0800

    change router.js

---
+++ static/js/about.0a69aeb4.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,n,e){"use strict";e.r(n);var a=function(){var t=this,n=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{staticClass:"about"},[e("h1",[t._v("This is an about page")])])}],c={created:function(){this.$axios.post("/info",{t:Math.random()}).then(function(t){})}},i=c,o=e("17cc"),u=Object(o["a"])(i,a,s,!1,null,null,null);n["default"]=u.exports}}]);
+//# sourceMappingURL=about.0a69aeb4.js.map
